e98113a6946df3b0494aad29c94f25f0fa3375de
The4thOrphan <s01325429@acad.tri-c.edu>
2025-05-02 16:58:00 +0000

    Create Lab7

---
+++ Lab7/Program.cs
@@ -0,0 +1,69 @@
+﻿class Book
+{
+    private string title;
+    private string author;
+
+    public Book()  
+    {
+        title = "Unknown Title";
+        author = "Unknown Author";
+    }
+
+    public Book(string newTitle)  
+    {
+        title = newTitle;
+        author = "Unknown Author";
+    }
+
+    public Book(string newTitle, string newAuthor)  
+    {
+        title = newTitle;
+        author = newAuthor;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public void SetTitle(string newTitle)
+    {
+        title = newTitle;
+    }
+
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    public void SetAuthor(string newAuthor)
+    {
+        author = newAuthor;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book();
+        Console.WriteLine(book1.GetTitle());  
+
+        book1.SetTitle("C# Fundamentals");
+        Console.WriteLine(book1.GetTitle());  
+
+        Book book2 = new Book("Advanced C#");
+        Console.WriteLine(book2.GetTitle());  
+
+        Book book3 = new Book("Mastering C#", "Cee Sharpe");
+        Console.WriteLine(book3.GetTitle());  
+        Console.WriteLine(book3.GetAuthor());  
+
+        Book book4 = new Book();  
+        book4.SetTitle("C# for Beginners");
+        book4.SetAuthor("Homer Simpson");
+
+        Console.WriteLine(book4.GetTitle());  
+        Console.WriteLine(book4.GetAuthor());  
+    }
+}
