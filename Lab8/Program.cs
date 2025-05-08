0a0ecd24eb3a86d7ea2bfda5e60aa049eb267d65
The4thOrphan <s01325429@acad.tri-c.edu>
2025-05-08 14:48:58 +0000

    Create Lab 8

---
+++ Lab8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab8;
+
+using System;
+
+class Animal  
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+class Cat : Animal  
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+class Dog : Animal  
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal mamimal = new Animal(); 
+        Animal theFelinecompanion = new Cat();      
+        Animal Bog = new Dog();       
+
+        mamimal.animalSound(); 
+        theFelinecompanion.animalSound();      
+        Bog.animalSound();      
+    }
+}
